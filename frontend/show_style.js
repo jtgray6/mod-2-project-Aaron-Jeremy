ae526d6c722c2ed3b3744fac1e08e87bf0f4be87
Jeremy Gray <jeremytgray6@gmail.com>
2020-02-13 13:09:36 -0700

    add show_style page and styling for style cards

--- frontend/show_style.js
+++ frontend/show_style.js
@@ -7,15 +7,41 @@ const user_id = params.get('user_id')
 const $section = document.querySelector('section')
 
 
-fetch(`http://localhost:4000/styles/${id}`)
-    .then(response => response.json())
-    .then(style => {
-        title = document.createElement('h2')
-        title.innerText = `${style.name}s:`
-        $section.append(title)
-        style.beers.map (beer => {
-            const beer_name = document.createElement('h3')
-            beer_name.innerHTML = `<a href='http://localhost:3000/show_beer.html?id=${beer.id}&user=${user_id}'>${beer.name}</a>`
-            $section.append(beer_name)
+
+
+if (id != null) {
+    fetch(`http://localhost:4000/styles/${id}`)
+        .then(response => response.json())
+        .then(style => {
+            const title = document.createElement('h2')
+            const description = document.createElement('h4')
+            const beers_head = document.createElement('h2')
+            description.innerText = `${style.description}`
+            description.className = "style_desc"
+            title.innerText = `${style.name} Info:`
+            title.className = "style_title"
+            beers_head.innerText = "Beers of this Style:"
+            beers_head.className = "beers_head"
+            $section.append(title, description, beers_head)
+            style.beers.map (beer => {
+                const beer_name = document.createElement('li')
+                beer_name.innerHTML = `<a href='http://localhost:3000/show_beer.html?id=${beer.id}&user=${user_id}'>${beer.name}</a>`
+                beer_name.className = "style_beer"
+                $section.append(beer_name)
+            })
+            const image = document.createElement('img')
+            image.src = `${style.image_url}`
+            $section.append(image)
         })
-    })
+    }
+else {
+    fetch('http://localhost:4000/styles')
+        .then(response => response.json())
+        .then(styles => {
+            styles.map (style => {
+                const style_name = document.createElement('h2')
+                style_name.innerHTML = `<a href='http://localhost:3000/show_style.html?id=${style.id}'>${style.name}</a>`
+                $section.append(style_name)
+            })
+        })
+}
